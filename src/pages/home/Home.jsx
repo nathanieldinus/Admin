5d7b839e63e353e1c227ca2686d139be5791edc8
nathanieldinus <tvpeace7@gmail.com>
2024-04-11 07:05:43 +0700

    Update navbar + chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,26 @@
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">container</div>
-      <Navbar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Chart type="order" aspect={2/1} />
+          <Chart type="earning" aspect={2/1} />
+        </div>
+      </div>
     </div>
   );
 };
