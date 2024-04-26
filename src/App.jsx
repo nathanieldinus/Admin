5490f35960a0e8d0971e81a625cc5c1803ec790a
nathanieldinus <tvpeace7@gmail.com>
2024-04-26 16:43:00 +0700

    Update List dan Single

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,9 +11,15 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/">
+        <Route path="/">
             <Route index element={<Home />}></Route>
-          </Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="users">
+              <Route index element={<List />}></Route>
+              <Route path=":userId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+        </Route>
         </Routes>
       </BrowserRouter>
     </>
