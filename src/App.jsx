0065faef7115920a2990f22e9f93b32c6d11bb7e
nathanieldinus <tvpeace7@gmail.com>
2024-06-07 16:29:03 +0700

    Update login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import { productInputs, userInputs } from "./formsource";
 import Categories from "./pages/categories/Categories";
 import "./style/dark.scss";
@@ -29,17 +29,17 @@ function App() {
       <BrowserRouter>
         <Routes>
         <Route path="/">
-            <Route index element={<Home />}></Route>
-            <Route path="login" element={<Login />}></Route>
+           <Route path="login" element={<NotRequireAuth><Login /></NotRequireAuth>}></Route>
+            <Route index element={<RequireAuth><Home /></RequireAuth>}></Route>
             <Route path="users">
-              <Route index element={<List />}></Route>
-              <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New inputs={userInputs} title="Add New User"/>}/>
+              <Route index element={<RequireAuth><List /></RequireAuth>}></Route>
+              <Route path=":userId" element={<RequireAuth><Single /></RequireAuth>}></Route>
+              <Route path="new" element={<RequireAuth><New inputs={userInputs} title="Add New User" /></RequireAuth>}/>
             </Route>
             <Route path="products">
-              <Route index element={<List />}></Route>
-              <Route path="productId" element={<Single />}></Route>
-              <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>}/>
+              <Route index element={<RequireAuth><List /></RequireAuth>}></Route>
+              <Route path="productId" element={<RequireAuth><Single /></RequireAuth>}></Route>
+              <Route path="new" element={<RequireAuth><New inputs={productInputs} title="Add New Product" /></RequireAuth>}/>
             </Route>
             <Route path="categories">
               <Route index element={<Categories/> }></Route>
